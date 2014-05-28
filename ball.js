2563c800a6d6a09a4fafa4476bd656b21087a99d
Maxence <maxence.bobin@hotmail.fr>
2014-05-28 14:02:11 +0200

    dsf

--- ball.js
+++ ball.js
@@ -1,6 +1,6 @@
 function ballPhysics() {
         // if ball goes off the 'left' side (Player's side)
-        if (ball.position.x <= -fieldWidth/2) {
+        if (ball.position.y <= -fieldHeight/2) {
                 // CPU scores
                 //score2++;
                 // update scoreboard HTML
@@ -11,7 +11,7 @@ function ballPhysics() {
                 //matchScoreCheck();
         }
         // if ball goes off the 'right' side (CPU's side)
-        if (ball.position.x >= fieldWidth/2) {       
+        if (ball.position.y >= fieldHeight/2) {       
                 // Player scores
                 //score1++;
                 // update scoreboard HTML
@@ -22,12 +22,12 @@ function ballPhysics() {
                 //matchScoreCheck();      
         }
         // if ball goes off the top side (side of table)
-        if (ball.position.y <= -fieldHeight/2) {
-                ballDirY = -ballDirY;
+        if (ball.position.x <= -fieldWidth/2) {
+                ballDirX = -ballDirX;
         }       
         // if ball goes off the bottom side (side of table)
-        if (ball.position.y >= fieldHeight/2) {
-                ballDirY = -ballDirY;
+        if (ball.position.x >= fieldWidth/2) {
+                ballDirX = -ballDirX;
         }
         
         // update ball position over time
@@ -37,11 +37,11 @@ function ballPhysics() {
         // limit ball's y-speed to 2x the x-speed
         // this is so the ball doesn't speed from left to right super fast
         // keeps game playable for humans
-        if (ballDirY > ballSpeed * 2) {
-                ballDirY = ballSpeed * 2;
+        if (ballDirX > ballSpeed * 2) {
+                ballDirX = ballSpeed * 2;
         }
-        else if (ballDirY < -ballSpeed * 2) {
-                ballDirY = -ballSpeed * 2;
+        else if (ballDirX < -ballSpeed * 2) {
+                ballDirX = -ballSpeed * 2;
         }
 }
 
@@ -53,15 +53,15 @@ function resetBall(loser) {
 
         // if player lost the last point, we send the ball to opponent
         if (loser == 1) {
-                ballDirX = -1;
+                ballDirY = -1;
         }
         // else if opponent lost, we send ball to player
         else {
-                ballDirX = 1;
+                ballDirY = 1;
         }
 
         // set the ball to move +ve in y plane (towards left from the camera)
-        ballDirY = 1;
+        ballDirX = 1;
 }
 
 function paddlePhysics()
@@ -71,24 +71,24 @@ function paddlePhysics()
 	// if ball is aligned with paddle1 on x plane
 	// remember the position is the CENTER of the object
 	// we only check between the front and the middle of the paddle (one-way collision)
-	if (ball.position.x <= raquette1.position.x + paddleWidth
-	&& ball.position.x >= raquette1.position.x)
+	if (ball.position.y <= raquette1.position.y + paddleHeight
+	&& ball.position.y >= raquette1.position.y)
 	{
 		// and if ball is aligned with paddle1 on y plane
-		if (ball.position.y <= raquette1.position.y + paddleHeight/2
-		&& ball.position.y >= raquette1.position.y - paddleHeight/2)
+		if (ball.position.x <= raquette1.position.x + paddleWidth/2
+		&& ball.position.x >= raquette1.position.x - paddleWidth/2)
 		{
 			// and if ball is travelling towards player (-ve direction)
-			if (ballDirX < 0)
+			if (ballDirY < 0)
 			{
 			// stretch the paddle to indicate a hit
 			//raquette1.scale.y = 15;
 			// switch direction of ball travel to create bounce
-			ballDirX = -ballDirX;
+			ballDirY = -ballDirY;
 			// we impact ball angle when hitting it
 			// this is not realistic physics, just spices up the gameplay
 			// allows you to 'slice' the ball to beat the opponent
-			ballDirY -= raquette1DirY * 0.7;
+			ballDirX -= raquette1DirY * 0.7;
 			}
 		}
 	}
@@ -98,24 +98,24 @@ function paddlePhysics()
 	// if ball is aligned with paddle2 on x plane
 	// remember the position is the CENTER of the object
 	// we only check between the front and the middle of the paddle (one-way collision)
-	if (ball.position.x <= raquette2.position.x + paddleWidth
-	&& ball.position.x >= raquette2.position.x)
+	if (ball.position.y <= raquette2.position.y + paddleHeight
+	&& ball.position.y >= raquette2.position.y)
 	{
 		// and if ball is aligned with paddle2 on y plane
-		if (ball.position.y <= raquette2.position.y + paddleHeight/2
-		&& ball.position.y >= raquette2.position.y - paddleHeight/2)
+		if (ball.position.x <= raquette2.position.x + paddleWidth/2
+		&& ball.position.x >= raquette2.position.x - paddleWidth/2)
 		{
 			// and if ball is travelling towards opponent (+ve direction)
-			if (ballDirX > 0)
+			if (ballDirY > 0)
 			{
 				// stretch the paddle to indicate a hit
 				//raquette2.scale.y = 15;	
 				// switch direction of ball travel to create bounce
-				ballDirX = -ballDirX;
+				ballDirY = -ballDirY;
 				// we impact ball angle when hitting it
 				// this is not realistic physics, just spices up the gameplay
 				// allows you to 'slice' the ball to beat the opponent
-				ballDirY -= raquette2DirY * 0.7;
+				ballDirX -= raquette2DirY * 0.7;
 			}
 		}
 	}
